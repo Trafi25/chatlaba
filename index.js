61f04a32b624379b41ee3162ecff072d93cdbc09
Vlad gribovskiy <45690086+Trafi25@users.noreply.github.com>
2019-12-19 19:43:49 +0200

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+
+http.listen(3000, () => {
+    console.log('Listening on port *: 3000');
+});
+
+io.on('connection', (socket) => {
+    console.log("A user connected");
+
+    socket.emit('connections', Object.keys(io.sockets.connected).length);
+
+    socket.on('disconnect', () => {
+        console.log("A user disconnected");
+    });
+
+    socket.on('chat-message', (data) => {
+        socket.broadcast.emit('chat-message', (data));
+    });   
+
+});
